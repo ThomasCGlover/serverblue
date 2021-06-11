9e04de56c431c01ffa48c5b8b673779d82dc1c42
Thomas Glover <thomasglover@Thomass-MacBook-Pro.local>
2021-06-11 09:06:17 -0400

    added user endpoints

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const Express = require("express");
+const Express = require('express');
 const app = Express();
+const dbConnection = require("./db");
 
-app.use('/test', (req, res) =>{
-    res.send("This is a message from the test endpoint on the server!")
-});
+app.use(Express.json());
+const controllers = require('./controllers');
 
-app.listen(3000, () =>{
-    console.log(`[Server]: App is listening on 3000`);
-});
+app.use("/user", controllers.userController);
+
+dbConnection.authenticate()
+    .then(() => dbConnection.sync())
+    .then(() => {
+        app.listen(3000, () => {
+            console.log(`[Server]: App is listening on 3000.`)
+        });
+    })
+    .catch((err) => {
+        console.log(`[Server]: Server crashed. Error = ${err}`)
+    });
