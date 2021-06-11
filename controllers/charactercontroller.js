f823ef057cb839b11b9396850df863cdf1bb882e
Nick Barnes <Nicholas.Barnes4194@.com>
2021-06-11 07:55:53 -0500

    added character update endpoint working as well

--- controllers/charactercontroller.js
+++ controllers/charactercontroller.js
@@ -6,6 +6,7 @@ const CharacterModel = require('../models/character')
 router.post("/create", async (req,res) =>{
     console.log(req.body);
     const {charName, charClass, race, STR, DEX, CON, INT, WIS, CHA, description,background, campaign} =req.body.character;
+    // const { id } = req.user;
 
     const charCreate = {
         charName,
@@ -34,6 +35,24 @@ router.post("/create", async (req,res) =>{
         } catch(err){
         res.status(500).json({
             message:`Failed to create Character: ${err}`
+        })
+    }
+})
+
+router.put('/:id', async (req, res) =>{
+    const {charName, charClass, race, STR, DEX, CON, INT, WIS, CHA, description,background, campaign} =req.body.character;
+    try{
+        const charUpdate = await CharacterModel.update({
+            charName, charClass, race, STR, DEX, CON, INT, WIS, CHA, description,background, campaign},
+            {where: {id: req.params.id}}
+            )
+            res.status(200).json({
+                message: `Character successfully updated`,
+                charUpdate
+            })
+    }catch(err) {
+        resizeBy.status(500).json({
+            message: `Failed to update Character: ${err}`
         })
     }
 })
