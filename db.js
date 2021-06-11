a477789447ac2f089d259f2edc0ae33b023503e7
Nick Barnes <Nicholas.Barnes4194@.com>
2021-06-11 08:37:42 -0500

    backup of user and character functionality

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require('sequelize');
 
-const sequelize = new Sequelize("postgres://postgres:cletus123@localhost:5432/dnd-users")
+const sequelize = new Sequelize("postgres://postgres:password@localhost:5432/dnd-users")
 
 module.exports = sequelize;
 
