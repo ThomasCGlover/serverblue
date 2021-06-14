dae583d81db83b50805529d76ce10d69132aab6d
Thomas Glover <thomasglover@Thomass-MacBook-Pro.local>
2021-06-14 15:56:29 -0400

    first heroku deployment attempt

--- db.js
+++ db.js
@@ -4,6 +4,7 @@ const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING, {
     dialect: 'postgres',
+    ssl: process.env.ENVIRONMENT === 'production'
 })
 
 module.exports = sequelize;
